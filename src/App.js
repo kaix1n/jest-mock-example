5698b86a4555127e173e59efdcb1543f3ce760e3
Kaixin <jiuyue@outlook.com>
2019-06-16 17:09:07 +0800

    Setup enzyme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const fetch = keywords =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      const index = ['- 1', '- 2', '- 3'];
+      const response = index.map((item, i) => `${keywords} ${item}`);
+      return resolve(response);
+    }, 10000);
+  });
+
+const Loading = () => 'loading';
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const onInputChange = event => setInputValue(event.target.value);
+
+  useEffect(() => {
+    if (inputValue) {
+      let cancel = false;
+      setLoading(true);
+      fetch(inputValue).then(response => {
+        if (!cancel) {
+          console.log({ response });
+          cancel = true;
+          setSearchResult(response);
+          setLoading(false);
+        }
+      });
+      return () => {
+        cancel = true;
+      };
+    } else {
+      setSearchResult([]);
+    }
+  }, [inputValue]);
+
+  const result = loading ? (
+    <Loading />
+  ) : (
+    searchResult.map((item, i) => <div key={i}>{item}</div>)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={inputValue} onChange={onInputChange} />
+      {result}
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import App from './App';
+
+jest.useFakeTimers();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('update input', () => {
+  const app = mount(<App />);
+  act(() => {
+    app.find('input').prop('onChange')({
+      target: {
+        value: 'keywords'
+      }
+    });
+  });
+  app.update();
+  expect(app.find('input').prop('value')).toEqual('keywords');
+});
+
+it('fetch data', () => {
+  const app = mount(<App />);
+  act(() => {
+    app.find('input').prop('onChange')({
+      target: {
+        value: 'keywords'
+      }
+    });
+  });
+  app.update();
+  expect(app.find('input').prop('value')).toEqual('keywords');
+  expect(app.find('Loading')).not.toBeNull();
+
+  act(() => {
+    jest.runAllTimers();
+    app.update();
+  });
+
+  console.log(app.debug());
 });
